b38f19245dbcc0113b866980fee7bac4529156ca
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-03-21 18:59:22 -0300

    feat(api): get profile user

--- apps/web/src/service/product.ts
+++ apps/web/src/service/product.ts
@@ -5,7 +5,9 @@ import { apiServer } from './apiServer'
 type Product = Pick<IProduct, 'id'>
 
 export async function getProduct({ id }: Product) {
-  const data = await apiServer<IProduct>(`/products/${id}`)
+  const data = await apiServer<IProduct>(`/products/${id}`, {
+    cache: 'force-cache',
+  })
 
   return data
 }
