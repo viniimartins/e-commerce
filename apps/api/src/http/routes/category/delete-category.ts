7a72234f4cf33480a07e0594871d8e7afe198b8c
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-04-03 16:27:55 -0300

    feat(api): delete category

---
+++ apps/api/src/http/routes/category/delete-category.ts
@@ -0,0 +1,50 @@
+import { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function deleteCategory(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      '/category/:id',
+      {
+        schema: {
+          tags: ['Category'],
+          summary: 'Delete category',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            id: z.string(),
+          }),
+          response: {
+            200: z.null(),
+            400: z.object({
+              message: z.string(),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { id } = request.params
+
+        const category = await prisma.category.findUnique({
+          where: { id },
+        })
+
+        if (!category) {
+          throw new BadRequestError('Category not found.')
+        }
+
+        await prisma.category.delete({
+          where: { id },
+        })
+
+        return reply.status(200)
+      },
+    )
+}
--- apps/api/src/http/routes/category/post-caterory.ts
+++ apps/api/src/http/routes/category/post-caterory.ts
@@ -2,37 +2,41 @@ import { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
 
+import { auth } from '@/http/middlewares/auth'
 import { prisma } from '@/lib/prisma'
 
 export async function postCategory(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().post(
-    '/category',
-    {
-      schema: {
-        tags: ['Category'],
-        summary: 'Create category',
-        body: z.object({
-          name: z.string().min(2, {
-            message: 'Nome da categoria deve ter pelo menos 2 caracteres.',
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/category',
+      {
+        schema: {
+          tags: ['Category'],
+          summary: 'Create category',
+          body: z.object({
+            name: z.string().min(2, {
+              message: 'Nome da categoria deve ter pelo menos 2 caracteres.',
+            }),
           }),
-        }),
-        response: {
-          201: z.object({
-            id: z.string(),
-          }),
+          response: {
+            201: z.object({
+              id: z.string(),
+            }),
+          },
         },
       },
-    },
-    async (request, reply) => {
-      const { name } = request.body
+      async (request, reply) => {
+        const { name } = request.body
 
-      const category = await prisma.category.create({
-        data: {
-          name,
-        },
-      })
+        const category = await prisma.category.create({
+          data: {
+            name,
+          },
+        })
 
-      return reply.status(201).send({ id: category.id })
-    },
-  )
+        return reply.status(201).send({ id: category.id })
+      },
+    )
 }
