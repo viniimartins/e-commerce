74b595a0174a452ae5d457c1a6bc168b75a42363
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-04-03 11:38:48 -0300

    feat(api): post category

---
+++ apps/api/src/http/routes/category/post-caterory.ts
@@ -0,0 +1,36 @@
+import { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function postCategory(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/category',
+    {
+      schema: {
+        tags: ['Category'],
+        summary: 'Create category',
+        body: z.object({
+          name: z.string(),
+        }),
+        response: {
+          201: z.object({
+            id: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name } = request.body
+
+      const category = await prisma.category.create({
+        data: {
+          name,
+        },
+      })
+
+      return reply.status(201).send({ id: category.id })
+    },
+  )
+}
